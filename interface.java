0e166b27261096d8b22c9c850190423880aab39b
Abhishek joji <148741327+AJ26WEST@users.noreply.github.com>
2024-10-11 09:44:54 +0530

    Update interface.java

--- interface.java
+++ interface.java
@@ -1,92 +1,139 @@
-class Animal{
+/*
+ * Name: ABHISHEK JOJI
+ * Date: 30-09-2024
+ * Purpose: Write a Java program that implements an interface and demonstrates the concept of inheritance.
+ *          This program defines various animals with specific behaviors and integrates the Pet interface for some animals.
+ */
+
+class Animal {
     int legs;
-    Animal(int legs){
+
+    // Constructor to initialize the number of legs for an animal
+    Animal(int legs) {
         this.legs = legs;
     }
-    void walk(){
-        System.out.print("\nThis animal walks on"+ legs + "legs");
-    }
-    void eats(){
-        System.out.print("\n This animal eats ");
+
+    // Method to simulate walking behavior for the animal
+    void walk() {
+        System.out.print("\nThis animal walks on " + legs + " legs");
     }
 
+    // Method to simulate the eating behavior of the animal
+    void eats() {
+        System.out.print("\nThis animal eats");
+    }
 }
 
-interface Pet{
+// Interface Pet with methods to be implemented by pet animals
+interface Pet {
     String getName();
     void setName(String name);
     void play();
 }
-class Spider extends Animal{
-    Spider(int legs){
+
+// Class Spider extends Animal but does not implement Pet
+class Spider extends Animal {
+    Spider(int legs) {
         super(legs);
     }
-    void eat(){
-        System.out.print("\n Spider eats insects.");
+
+    // Spider's specific eat method
+    void eat() {
+        System.out.print("\nSpider eats insects.");
     }
 }
-class Cat extends Animal implements Pet{
+
+// Class Cat extends Animal and implements Pet interface
+class Cat extends Animal implements Pet {
     String name;
-    Cat(String name, int legs){
+
+    // Constructor for Cat that initializes its name and number of legs
+    Cat(String name, int legs) {
         super(legs);
         this.name = name;
     }
-    public String getName(){
+
+    // Implementation of getName method from Pet interface
+    public String getName() {
         return name;
     }
-    public void setName(String name){
+
+    // Implementation of setName method from Pet interface
+    public void setName(String name) {
         this.name = name;
     }
-    public void play(){
+
+    // Implementation of play method from Pet interface
+    public void play() {
         System.out.print("\nCat loves to play with ball");
     }
-    public void eats(){
-        System.out.print("\n Cat eats fish");
+
+    // Override eat method to provide Cat-specific behavior
+    public void eats() {
+        System.out.print("\nCat eats fish");
     }
 }
 
-class Fish extends Animal implements Pet{
+// Class Fish extends Animal and implements Pet interface
+class Fish extends Animal implements Pet {
     String name;
-    Fish(String name, int legs){
+
+    // Constructor for Fish that initializes its name and number of legs
+    Fish(String name, int legs) {
         super(legs);
-        this.name=name;
-    }
-    public String getName(){
-        return name;
-    }
-    public void setName(String name){
         this.name = name;
     }
-    public void eats(){
-        System.out.print("\n Fish eats plants");
+
+    // Implementation of getName method from Pet interface
+    public String getName() {
+        return name;
     }
-    public void play(){
-        System.out.print("\n Fish likes to play with shells");
+
+    // Implementation of setName method from Pet interface
+    public void setName(String name) {
+        this.name = name;
     }
-    public void walk(){
-        System.out.print("\n Fish has no legs");
+
+    // Override eat method to provide Fish-specific behavior
+    public void eats() {
+        System.out.print("\nFish eats plants");
+    }
+
+    // Implementation of play method from Pet interface
+    public void play() {
+        System.out.print("\nFish likes to play with shells");
+    }
+
+    // Override walk method since Fish doesn't walk like other animals
+    public void walk() {
+        System.out.print("\nFish has no legs");
     }
 }
-public class Interface{
-    public static void main(String[] args){
+
+public class Interface {
+    public static void main(String[] args) {
+        // Create an Animal object with 4 legs
         Animal a = new Animal(4);
 
+        // Working with Fish object
         System.out.print("\n\nFish");
-        Fish f = new Fish("Mimi",0);
-        System.out.print("\nThis fish's name is "+f.getName());
+        Fish f = new Fish("Mimi", 0);
+        System.out.print("\nThis fish's name is " + f.getName());
         f.eats();
         f.walk();
         f.setName("Nemo");
-        System.out.print("\nThis fish's name is "+f.getName());
+        System.out.print("\nThis fish's name is " + f.getName());
 
+        // Working with Cat object
         System.out.print("\n\nCat");
-        Cat c = new Cat("Tom",4);
-        System.out.print("\nThis cat's name is "+c.getName());
+        Cat c = new Cat("Tom", 4);
+        System.out.print("\nThis cat's name is " + c.getName());
         c.eats();
         c.walk();
         c.setName("Tim");
-        System.out.print("\nThis cat's name is "+c.getName());
+        System.out.print("\nThis cat's name is " + c.getName());
 
+        // Working with Spider object
         System.out.print("\n\nSpider");
         Spider s = new Spider(8);
         s.eat();
