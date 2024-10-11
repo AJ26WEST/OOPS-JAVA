be45c4170a58a562385e958235b39058e63112f8
Abhishek joji <148741327+AJ26WEST@users.noreply.github.com>
2024-10-11 09:41:25 +0530

    Update tcof.java

--- try catch/tcof.java
+++ try catch/tcof.java
@@ -1,28 +1,36 @@
+/*
+ * Name: ABHISHEK JOJI
+ * Date: 30-09-2024
+ * Purpose: Write a Java program that demonstrates the use of try, catch, and finally blocks.
+ *          The program verifies if a person is eligible to vote based on their age.
+ */
+
 import java.util.Scanner;
-class AgeVerify{
-       void vote (int age) throws IllegalAccessException{
-          try{
-               if(age<18){
-                  throw new IllegalAccessException("you must be at least 18 years old to vote.");
-               }else{
-                  System.out.println("you can vote!");
-               }
-          }catch (Exception e) {
-                System.out.println("exception occured:" + e);
-           }finally{
-                System.out.println("Finally age verification done.");
-           }
-       }
+
+class AgeVerify {
+    void vote(int age) throws IllegalAccessException {
+        try {
+            if (age < 18) {
+                throw new IllegalAccessException("You must be at least 18 years old to vote.");
+            } else {
+                System.out.println("You can vote!");
+            }
+        } catch (Exception e) {
+            System.out.println("Exception occurred: " + e);
+        } finally {
+            System.out.println("Finally, age verification done.");
+        }
+    }
 }
 
-public class usageoftry{
-      public static void main(String[] args) throws IllegalAccessException {
-             Scanner sc =new Scanner(System.in);
-             int age;
-             System.out.println("Enter age of voter:");
-             age =sc.nextInt();
-             AgeVerify av=new AgeVerify();
-             av.vote(age);
-             System.out.println("Verification completed successfully");
-        }
+public class usageoftry {
+    public static void main(String[] args) throws IllegalAccessException {
+        Scanner sc = new Scanner(System.in);
+        int age;
+        System.out.println("Enter age of voter:");
+        age = sc.nextInt();
+        AgeVerify av = new AgeVerify();
+        av.vote(age);
+        System.out.println("Verification completed successfully");
+    }
 }
